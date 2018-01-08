2a270b1c87dfe32ddec4a33708ec87fda51c5773
Amol Patel <amolpatel4@gmail.com>
2018-01-08 16:12:43 -0500

    added child to parent login connection

---
+++ src/containers/homePage.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { login, getUser } from '../actions/userActions';
+
+import InputField from '../components/inputField';
+import LoginContainer from '../components/loginContainer';
+
+
+
+class Login extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      error: ''
+    };
+
+    this.submitLogin = this.submitLogin.bind(this);
+
+  }
+
+  componentWillMount() {
+    if(this.props.user !== null) {
+      this.props.history.push('/');
+    }
+  }
+
+  submitLogin() {
+    this.props.login(this.state.email, this.state.password).catch(err => {
+      this.setState({
+        error: err
+      });
+    });
+  }
+
+  setEmail(event) {
+    this.setState({
+      email: event.target.value
+    });
+  }
+
+  setPassword(event) {
+    this.setState({
+      password: event.target.value
+    });
+  }
+
+  createAccountHandler() {
+
+  }
+
+  render() {
+    const errStyle = {
+      borderColor: 'red'
+    };
+    return (
+        <div className="container">
+          <div className="form-signin">
+            <h2 className="form-signin-heading">PasswordSafe</h2>
+
+            <LoginContainer email={this.setEmail.bind(this)} password={this.setPassword.bind(this)}/>
+
+
+            <a className="create-account" href="/createAccount">Create Account</a>
+            <br/>
+            <button
+                type="submit"
+                className="btn btn-primary login-button"
+                onClick={event => {this.submitLogin(event);}}>
+              Login
+            </button>
+          </div>
+        </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { user: state.user};
+}
+
+export default connect(mapStateToProps, { login, getUser })(Login);
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import reducers from './reducers/index'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './styles/App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 
 // ROUTING
 import { Provider } from 'react-redux'
@@ -19,7 +20,7 @@ import thunk from 'redux-thunk'
 import Loading from './containers/loading';
 import Authenticated from './containers/authenticated';
 import createAccount from './containers/createAccount';
-import login from './containers/login';
+import login from './containers/homePage';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
